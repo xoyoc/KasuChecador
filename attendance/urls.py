b51f88c6db346d5f30c18c1d875135a8198c27af
xoyoc <xoyoc_l2@hotmail.com>
2025-11-24 14:11:24 -0600

    correcion de urls

--- attendance/urls.py
+++ attendance/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     # Tablet de recepción
     path('checkin/', views.checkin_view, name='checkin'),
-    path('checkin_tablet/', views.checkin_view_tablet, name='checkin_tablet'),
+    path('', views.checkin_view_tablet, name='checkin_tablet'),
 
     # Registro de visitantes (público)
     path('visitante/registro/', views.VisitanteCreateView.as_view(), name='visitante_registro'),
